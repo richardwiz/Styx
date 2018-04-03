7633a2e4dd9380ddb3f21bed85fff11112a0e4cd
Richard Wiz <richardwisbey@yahoo.co.uk>
2018-04-03 11:18:00 +1000

    Add project files.

---
+++ AMQ.Subscriber.Client/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AMQ.Subscriber.Client
+{
+   class Program
+   {
+      static void Main(string[] args)
+      {
+      }
+   }
+}
